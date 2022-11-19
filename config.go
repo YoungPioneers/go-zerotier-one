8bcf45ff35f0764818da3e8038988b5233d97f37
huangjunwei <huangjunwei@youmi.net>
2022-11-19 22:29:50 +0800

    添加local.conf配置文件结构

--- config.go
+++ config.go
@@ -5,6 +5,8 @@ package zerotier
 const (
 	// DefaultAddress default zerotier-one address
 	DefaultAddress = "http://127.0.0.1:9993"
+	// LocalConfigPath zerotier local.conf default path
+	LocalConfigPath = "/var/lib/zerotier-one/local.conf"
 )
 
 // Config .
@@ -26,4 +28,41 @@ func (cfg *Config) WithAddress(address string) *Config {
 	cfg.Address = address
 
 	return cfg
+}
+
+// LocalConfig .
+type LocalConfig struct {
+	Physical map[string]NetworkConfig `json:"physical,omitempty"`
+	Virtual  map[string]VirtualConfig `json:"virtual,omitempty"`
+	Settings SettingsConfig           `json:"settings,omitempty"`
+}
+
+// NetworkConfig .
+type NetworkConfig struct {
+	BlackList     bool  `json:"blacklist,omitempty"`
+	TrustedPathID int64 `json:"trustedPathId,omitempty"`
+	MTU           int64 `json:"mtu,omitempty"`
+}
+
+// VirtualConfig .
+type VirtualConfig struct {
+	Try       []string `json:"try,omitempty"`
+	BlackList []string `json:"blacklist,omitempty"`
+}
+
+// SettingsConfig .
+type SettingsConfig struct {
+	PrimaryPort              int64    `json:"primaryPort,omitempty"`
+	SecondaryPort            int64    `json:"secondaryPort,omitempty"`
+	TertiaryPort             int64    `json:"tertiaryPort,omitempty"`
+	PortMappingEnabled       bool     `json:"portMappingEnabled,omitempty"`
+	AllowSecondaryPort       bool     `json:"allowSecondaryPort,omitempty"`
+	SoftwareUpdate           string   `json:"softwareUpdate,omitempty"`
+	SoftwareUpdateChannel    string   `json:"softwareUpdateChannel,omitempty"`
+	SoftwareUpdateDist       bool     `json:"softwareUpdateDist,omitempty"`
+	InterfacePrefixBlacklist []string `json:"interfacePrefixBlacklist,omitempty"`
+	AllowManagementFrom      []string `json:"allowManagementFrom,omitempty"`
+	Bind                     []string `json:"bind,omitempty"`
+	AllowTCPFallbackRelay    bool     `json:"allowTcpFallbackRelay,omitempty"`
+	MultipathMode            int64    `json:"multipathMode,omitempty"`
 }
