cb247b3332a1ae740527ce77da340a6db957a0eb
huangjunwei <huangjunwei@youmi.net>
2022-12-01 17:12:45 +0800

    补充network rules字段

--- network.go
+++ network.go
@@ -4,33 +4,51 @@ package zerotier
 
 // Network .
 type Network struct {
-	AllowDNS          bool     `json:"allowDNS"`
-	AllowDefault      bool     `json:"allowDefault"`
-	AllowGlobal       bool     `json:"allowGlobal"`
-	AssignedAddresses []string `json:"assignedAddresses"`
-	Bridge            bool     `json:"bridge"`
-	BroadcastEnabled  bool     `json:"broadcastEnabled"`
-	DNS               struct {
-		Domain  string   `json:"domain"`
-		Servers []string `json:"servers"`
-	} `json:"dns"`
-	ID                     string `json:"id"`
-	MAC                    string `json:"mac"`
-	MTU                    uint   `json:"mtu"`
-	MulticastSubscriptions []struct {
-		ADI uint   `json:"adi"`
-		MAC string `json:"mac"`
-	} `json:"multicastSubscriptions"`
-	Name            string `json:"name"`
-	NetconfRevision uint   `json:"netconfRevision"`
-	PortDeviceName  string `json:"portDeviceName"`
-	PortError       uint   `json:"portError"`
-	Routes          []struct {
-		Flags  uint   `json:"flags"`
-		Metric uint   `json:"metric"`
-		Target string `json:"target"`
-		Via    string `json:"via"`
-	} `json:"routes"`
-	Status string `json:"status"`
-	Type   string `json:"type"`
+	AllowDNS               bool                           `json:"allowDNS,omitempty"`
+	AllowDefault           bool                           `json:"allowDefault,omitempty"`
+	AllowGlobal            bool                           `json:"allowGlobal,omitempty"`
+	AssignedAddresses      []string                       `json:"assignedAddresses,omitempty"`
+	Bridge                 bool                           `json:"bridge,omitempty"`
+	BroadcastEnabled       bool                           `json:"broadcastEnabled,omitempty"`
+	DNS                    NetworkDNS                     `json:"dns,omitempty"`
+	ID                     string                         `json:"id,omitempty"`
+	MAC                    string                         `json:"mac,omitempty"`
+	MTU                    uint                           `json:"mtu,omitempty"`
+	MulticastSubscriptions []NetworkMulticastSubscription `json:"multicastSubscriptions,omitempty"`
+	Name                   string                         `json:"name,omitempty"`
+	NetconfRevision        uint                           `json:"netconfRevision,omitempty"`
+	PortDeviceName         string                         `json:"portDeviceName,omitempty"`
+	PortError              uint                           `json:"portError,omitempty"`
+	Routes                 []NetworkRoute                 `json:"routes,omitempty"`
+	Rules                  []NetworkRule                  `json:"rules,omitempty"`
+	Status                 string                         `json:"status,omitempty"`
+	Type                   string                         `json:"type,omitempty"`
+}
+
+// NetworkDNS .
+type NetworkDNS struct {
+	Domain  string   `json:"domain,omitempty"`
+	Servers []string `json:"servers,omitempty"`
+}
+
+// NetworkMulticastSubscription .
+type NetworkMulticastSubscription struct {
+	ADI uint   `json:"adi,omitempty"`
+	MAC string `json:"mac,omitempty"`
+}
+
+// NetworkRoute .
+type NetworkRoute struct {
+	Flags  uint   `json:"flags,omitempty"`
+	Metric uint   `json:"metric,omitempty"`
+	Target string `json:"target,omitempty"`
+	Via    string `json:"via,omitempty"`
+}
+
+// NetworkRule .
+type NetworkRule struct {
+	EtherType uint   `json:"etherType,omitempty"`
+	Not       bool   `json:"not,omitempty"`
+	Or        bool   `json:"or,omitempty"`
+	Type      string `json:"type,omitempty"`
 }
